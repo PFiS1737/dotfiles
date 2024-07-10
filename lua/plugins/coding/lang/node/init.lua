f0ef586d45a3f86ba4ac90a4f05d6e24d1792901
PFiS1737 <pfis1737@gmail.com>
2024-07-10 21:46:43 +0800

    fix(filetype): add .swcrc as json

--- lua/plugins/coding/lang/node/init.lua
+++ lua/plugins/coding/lang/node/init.lua
@@ -1,4 +1,16 @@
 ---@type LazySpec
 return {
   { import = "plugins.coding.lang.node" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  {
+    "b0o/SchemaStore.nvim",
+    init = function()
+      vim.filetype.add({
+        filename = {
+          [".swcrc"] = "json",
+        },
+      })
+    end,
+  },
 }
