a5f25fff5966859799fd659311b6e86877de161f
PFiS1737 <pfis1737@gmail.com>
2024-07-10 21:45:47 +0800

    fix: remove vtsls diagnostics handlers

--- lua/plugins/coding/lang/node/typescript.lua
+++ lua/plugins/coding/lang/node/typescript.lua
@@ -11,26 +11,26 @@ return {
       servers = {
         vtsls = {
           settings = require("config.lsp.vtsls"),
-          handlers = {
-            ["textDocument/publishDiagnostics"] = function(err, res, ctx, config)
-              local codes = {
-                7044, -- Parameter 'xxx' implicitly has an 'any' type
-                7045, -- Member 'xxx' implicitly has an 'any[]' type
-                7047, -- Rest parameter 'xxx' implicitly has an 'any[]' type
-              }
-
-              local filtered = {}
-              for _, diag in ipairs(res.diagnostics) do
-                if not (diag.source == "ts" and vim.tbl_contains(codes, diag.code)) then
-                  -- print(diag.code)
-                  table.insert(filtered, diag)
-                end
-              end
-
-              res.diagnostics = filtered
-              vim.lsp.diagnostic.on_publish_diagnostics(err, res, ctx, config)
-            end,
-          },
+          -- handlers = {
+          --   ["textDocument/publishDiagnostics"] = function(err, res, ctx, config)
+          --     local codes = {
+          --       7044, -- Parameter 'xxx' implicitly has an 'any' type
+          --       7045, -- Member 'xxx' implicitly has an 'any[]' type
+          --       7047, -- Rest parameter 'xxx' implicitly has an 'any[]' type
+          --     }
+          --
+          --     local filtered = {}
+          --     for _, diag in ipairs(res.diagnostics) do
+          --       if not (diag.source == "ts" and vim.tbl_contains(codes, diag.code)) then
+          --         -- print(diag.code)
+          --         table.insert(filtered, diag)
+          --       end
+          --     end
+          --
+          --     res.diagnostics = filtered
+          --     vim.lsp.diagnostic.on_publish_diagnostics(err, res, ctx, config)
+          --   end,
+          -- },
         },
       },
     },
