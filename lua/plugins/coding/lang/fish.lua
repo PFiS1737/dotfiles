e7ebef9d1de0579133705156b16bdfda4d07ab3a
PFiS1737 <pfis1737@gmail.com>
2024-09-14 18:23:44 +0800

    feat: add fish lsp

--- lua/plugins/coding/lang/fish.lua
+++ lua/plugins/coding/lang/fish.lua
@@ -4,4 +4,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "fish" } },
   },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "ndonfris/fish-lsp",
+        build = "yarn install",
+      },
+    },
+    opts = {
+      servers = {
+        fish_lsp = {},
+      },
+    },
+  },
 }
