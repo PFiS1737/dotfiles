d61ce3bbf699d7938048885f1f5826a3cc75c267
PFiS1737 <pfis1737@gmail.com>
2024-09-19 18:07:25 +0800

    feat: use dropbar.nvim as breadcrumbs bar

--- lua/plugins/editor/ui/breadcrumbs.lua
+++ lua/plugins/editor/ui/breadcrumbs.lua
@@ -2,32 +2,52 @@
 return {
   {
     "Bekaboo/dropbar.nvim",
-    enabled = false,
     event = "LazyFile",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
-    },
-    opts = {},
-  },
-  {
-    "utilyre/barbecue.nvim",
-    event = "LazyFile",
-    dependencies = {
-      "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons",
     },
     keys = {
       {
+        "gB",
+        function()
+          require("dropbar.api").pick()
+        end,
+        desc = "Pick Breadcrumbs",
+      },
+      {
         "<leader>ub",
         function()
-          require("barbecue.ui").toggle()
+          ---@see https://github.com/Bekaboo/dropbar.nvim/issues/115
+          if vim.o.winbar == "" then
+            vim.o.winbar = "%{%v:lua.dropbar.get_dropbar_str()%}"
+          else
+            vim.o.winbar = ""
+          end
         end,
-        desc = "Toggle Breadcrumbs",
+        desc = "Toggle Breadcrumbs", -- FIXME: 由于 lazyvim.util.toggle.wk(), 此处的 desc 不生效
       },
     },
     opts = {
-      -- show_dirname = false,
-      -- show_basename = false
+      icons = {
+        ui = {
+          bar = {
+            separator = "  ",
+          },
+        },
+      },
+      menu = {
+        preview = false,
+        quick_navigation = false,
+        scrollbar = {
+          enable = false,
+        },
+      },
     },
+    config = function(_, opts)
+      require("dropbar").setup(opts)
+
+      vim.api.nvim_set_hl(0, "DropBarIconUISeparator", { link = "NormalSB" })
+    end,
   },
 }
