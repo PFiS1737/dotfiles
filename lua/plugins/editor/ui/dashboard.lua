e27e7c2ecf596a232ca4f6b023ef3b6844c66d20
PFiS1737 <pfis1737@gmail.com>
2024-11-19 21:49:06 +0800

    feat: update dashboard config

--- lua/plugins/editor/ui/dashboard.lua
+++ lua/plugins/editor/ui/dashboard.lua
@@ -1,23 +1,21 @@
--- figlet font file: https://github.com/xero/figlet-fonts/blob/master/ANSI%20Shadow.flf
--- preview: https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=neovim
-local logo = string.rep("\n", 8)
-  .. [[
+---@type LazySpec
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          -- figlet font file: https://github.com/xero/figlet-fonts/blob/master/ANSI%20Shadow.flf
+          -- preview: https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=neovim
+          header = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
 ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
 ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-]]
-  .. "\n"
-
----@type LazySpec
-return {
-  {
-    "nvimdev/dashboard-nvim",
-    opts = {
-      config = {
-        header = vim.split(logo, "\n"),
+]],
+        },
       },
     },
   },
