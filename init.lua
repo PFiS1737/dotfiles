62ad60652ea14164edb8d03ecaba40e6e7b494aa
PFiS1737 <pfis1737@gmail.com>
2024-12-03 22:18:41 +0800

    feat: update lazy.nvim setup

--- init.lua
+++ init.lua
@@ -1,6 +1,30 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
 if vim.tbl_contains(vim.v.argv, "+Man!") then
   -- use neovim as the man pager
   vim.opt.fillchars = { eob = " " }
+  require("config.man")
 else
   require("config.lazy")
 end
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,15 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--depth=1",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
@@ -18,24 +6,6 @@ require("lazy").setup({
   defaults = {
     lazy = true,
     version = false,
-    -- cond = function(self)
-    --   local event_has_LazyFile = (
-    --     type(self.event) == "table" and vim.tbl_contains(self.event, "LazyFile")
-    --     or self.event == "LazyFile"
-    --   ) and self[1] ~= "nvim-treesitter/nvim-treesitter"
-    --
-    --   local is_lualine_or_bufferline = self[1] == "nvim-lualine/lualine.nvim"
-    --     or self[1] == "akinsho/bufferline.nvim"
-    --
-    --   if
-    --     vim.tbl_contains(vim.v.argv, "+Man!")
-    --     and (event_has_LazyFile or is_lualine_or_bufferline)
-    --   then
-    --     return false
-    --   end
-    --
-    --   return true
-    -- end,
   },
   install = { colorscheme = { "tokyonight" } },
   checker = { enabled = false },
