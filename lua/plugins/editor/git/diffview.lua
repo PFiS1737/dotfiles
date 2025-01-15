8679cc06427415144f7e62a6ad70e9a75e57f7ce
PFiS1737 <pfis1737@gmail.com>
2025-01-15 17:44:17 +0800

    feat(git): update diffview config

--- lua/plugins/editor/git/diffview.lua
+++ lua/plugins/editor/git/diffview.lua
@@ -2,19 +2,15 @@
 return {
   {
     "sindrets/diffview.nvim",
-    cmd = {
-      "DiffviewOpen",
-      "DiffviewFileHistory",
-    },
     keys = {
       { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
       {
-        "<leader>gH",
+        "<leader>g<c-h>",
         "<cmd>DiffviewFileHistory<cr>",
         desc = "Open file history (current branch)",
       },
       {
-        "<leader>g<C-H>",
+        "<leader>gH",
         "<cmd>DiffviewFileHistory %<cr>",
         desc = "Open file history (current file)",
       },
@@ -32,7 +28,7 @@ return {
         { "n", "q", actions.close, { desc = "Close the file panel" } },
         {
           "n",
-          "<C-Q>",
+          "<c-q>",
           function()
             actions.close()
             actions.close()
@@ -57,7 +53,7 @@ return {
               actions.toggle_files,
               { desc = "Toggle file panel" },
             },
-            { "n", "<C-Q>", actions.close, { desc = "Close diffview" } },
+            { "n", "<c-q>", actions.close, { desc = "Close diffview" } },
           },
           file_panel = panel_keymaps,
           file_history_panel = panel_keymaps,
