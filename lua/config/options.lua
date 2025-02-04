a40aff42a2240d6736b8e13de352c2649f0d6c4d
PFiS1737 <pfis1737@gmail.com>
2025-02-04 13:11:08 +0800

    feat(statuscolumn): add left padding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,21 @@
 -- Default options: lazyvim.config.options
 
--- local opt = vim.opt
-
 if vim.env.TERMUX_VERSION ~= nil then
   vim.g.user_scroll_throttle = true
   vim.g.user_scroll_throttle_delay = 20
 end
 
+-- ==================
+--   Lazyvim Config
+-- ==================
 vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_blink_main = true
+
+-- ==================
+--   Neovim Options
+-- ==================
+
+local opt = vim.opt
+
+-- add left padding
+opt.statuscolumn = [[%!' '..v:lua.require'snacks.statuscolumn'.get()]]
