028bc3419cd4c192332bf58cb884681295a19c67
PFiS1737 <pfis1737@gmail.com>
2025-02-26 19:33:13 +0800

    feat(dap): add keybindding to start osv server

--- lua/plugins/coding/dap.lua
+++ lua/plugins/coding/dap.lua
@@ -2,4 +2,16 @@
 return {
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.dap.nlua" },
+  {
+    "jbyuki/one-small-step-for-vimkind",
+    keys = {
+      {
+        "<laeder>dL",
+        function()
+          require("osv").launch({ port = 8086 })
+        end,
+        desc = "Start OSV Server",
+      },
+    },
+  },
 }
