91e6952c743e8df31c7ffd209019aa076cc23233
PFiS1737 <pfis1737@gmail.com>
2025-03-15 16:34:13 +0800

    feat: add julia support

---
+++ lua/plugins/coding/lang/julia.lua
@@ -0,0 +1,30 @@
+---@type LazySpec
+return {
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "julia" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        julials = {
+          -- INFO: handled by mason-lspconfig
+          --
+          -- FIXME: need to install LanguageServer.jl by hand
+          --
+          -- LanguageServer.jl can be installed with `julia` and `Pkg`:
+          -- $ julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+          --
+          -- To update an existing install, use the following command:
+          -- $ julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()'
+          julia_env_path = "~/.julia/environments/nvim-lspconfig",
+          settings = {
+            julia = {},
+          },
+        },
+      },
+    },
+  },
+}
