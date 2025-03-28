32c7a2adb57c659a6afd0f72de1da4498fc5b027
PFiS1737 <pfis1737@gmail.com>
2025-03-28 22:56:08 +0800

    feat(lang.typst): remove utf-8 compat via upstream

--- lua/plugins/coding/lang/typst.lua
+++ lua/plugins/coding/lang/typst.lua
@@ -9,9 +9,6 @@ return {
     opts = {
       servers = {
         tinymist = {
-          -- fix chinese character issue
-          -- @see https://github.com/neovim/neovim/issues/30675
-          offset_encoding = "utf-8",
           single_file_support = true,
           settings = {
             exportPdf = "onSave", -- onType, onSave or never.
