1c7d36888492aec03f7b58566b094dc9e368842b
PFiS1737 <pfis1737@gmail.com>
2025-06-09 19:52:53 +0800

    fix: don't setup cmake dap target

--- lua/plugins/coding/lang/clang.lua
+++ lua/plugins/coding/lang/clang.lua
@@ -8,39 +8,39 @@ return {
       servers = {
         clangd = {
           mason = false, -- installed by system package manager
-          on_attach = function()
-            local cwd = LazyVim.root()
-            local dap = require("dap")
-
-            local file = io.open(cwd .. "/CMakeLists.txt", "r")
-            if not file then
-              return
-            end
-
-            local executable_names = {}
-            for line in file:lines() do
-              local name = line:match("add_executable%((%S+)%s")
-              if name then
-                vim.list_extend(executable_names, { name })
-              end
-            end
-
-            file:close()
-
-            for _, lang in ipairs({ "c", "cpp" }) do
-              for _, name in ipairs(executable_names) do
-                vim.list_extend(dap.configurations[lang], {
-                  {
-                    type = "codelldb",
-                    request = "launch",
-                    name = "Launch cmake executable `" .. name .. "`",
-                    program = cwd .. "/build/" .. name,
-                    cwd = cwd,
-                  },
-                })
-              end
-            end
-          end,
+          -- on_attach = function()
+          --   local cwd = LazyVim.root()
+          --   local dap = require("dap")
+          --
+          --   local file = io.open(cwd .. "/CMakeLists.txt", "r")
+          --   if not file then
+          --     return
+          --   end
+          --
+          --   local executable_names = {}
+          --   for line in file:lines() do
+          --     local name = line:match("add_executable%((%S+)%s")
+          --     if name then
+          --       vim.list_extend(executable_names, { name })
+          --     end
+          --   end
+          --
+          --   file:close()
+          --
+          --   for _, lang in ipairs({ "c", "cpp" }) do
+          --     for _, name in ipairs(executable_names) do
+          --       vim.list_extend(dap.configurations[lang], {
+          --         {
+          --           type = "codelldb",
+          --           request = "launch",
+          --           name = "Launch cmake executable `" .. name .. "`",
+          --           program = cwd .. "/build/" .. name,
+          --           cwd = cwd,
+          --         },
+          --       })
+          --     end
+          --   end
+          -- end,
         },
       },
     },
