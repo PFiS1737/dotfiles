911bb0851a1add43e4d00127ffa30bff9aec7b7d
PFiS1737 <pfis1737@gmail.com>
2025-06-09 19:55:36 +0800

    deps: remove nvim-notify dependency of leetcode.nvim

--- lua/plugins/coding/leetcode.lua
+++ lua/plugins/coding/leetcode.lua
@@ -2,18 +2,14 @@
 return {
   {
     "kawre/leetcode.nvim",
-    -- enabled = false,
-    build = ":TSUpdate html",
     cmd = { "Leet" },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "rcarriga/nvim-notify",
       "nvim-tree/nvim-web-devicons",
     },
     opts = {
-      -- arg = "leetcode",
       lang = "typescript",
     },
   },
