57ac2d05c5e8e253f451e4b911038a1dca11e734
PFiS1737 <pfis1737@gmail.com>
2025-07-26 09:21:39 +0800

    feat(todo-comment): separate `NOTE` and `INFO`

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -2,4 +2,14 @@
 ---@type LazySpec
 return {
   { import = "lazyvim.plugins.extras.coding.neogen" },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      -- merge_keywords = false, -- enable custom keywords
+      keywords = {
+        NOTE = { icon = " ", color = "hint", alt = {} },
+        INFO = { icon = " ", color = "info" },
+      },
+    },
+  },
 }
