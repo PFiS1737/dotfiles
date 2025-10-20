6025b8033b72b8429594bb7e8c698c167a4f417c
PFiS1737 <pfis1737@gmail.com>
2025-10-20 19:10:45 +0800

    refactor(typst): use LazyVim builtin typst extra

--- lua/plugins/coding/lang/typst.lua
+++ lua/plugins/coding/lang/typst.lua
@@ -1,19 +1,14 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "typst" } },
-  },
+  { import = "lazyvim.plugins.extras.lang.typst" },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         tinymist = {
-          single_file_support = true,
           settings = {
-            exportPdf = "onSave", -- onType, onSave or never.
-            formatterMode = "typstyle",
+            -- exportPdf = "onSave", -- onType, onSave or never.
           },
         },
       },
