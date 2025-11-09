6605038f02875e3dc4b30ce33ecd3a708fbf3a5b
PFiS1737 <pfis1737@gmail.com>
2025-11-09 23:02:14 +0800

    fix(action-preview): check `preview ~= nil`

--- lua/plugins/editor/ui/actions-preview.lua
+++ lua/plugins/editor/ui/actions-preview.lua
@@ -29,6 +29,18 @@ return {
       snacks = {
         preview = function(ctx)
           ctx.item.action:preview(function(preview)
+            -- Some lsp (e.g. astro) will provide all code actions in any case
+            -- while some of them were disabled.
+            if not preview then
+              local reason = vim.tbl_get(ctx.item.action, "action", "disabled", "reason")
+              if reason then
+                ctx.preview:notify(reason, "info", { item = false })
+              else
+                ctx.preview:notify("Preview not available", "error", { item = true })
+              end
+              return
+            end
+
             if preview.cmdline then
               ctx.preview:notify(
                 "Please keep `highlight_command` empty to use Snacks' builtin diff preview.",
