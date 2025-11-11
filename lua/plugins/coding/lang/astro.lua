1c97d7eafc30da70f70d7b88a1e2358b3a8e9044
PFiS1737 <pfis1737@gmail.com>
2025-11-11 22:59:31 +0800

    feat(astro): format with prettier for astro files

--- lua/plugins/coding/lang/astro.lua
+++ lua/plugins/coding/lang/astro.lua
@@ -2,4 +2,19 @@
 ---@type LazySpec
 return {
   { import = "lazyvim.plugins.extras.lang.astro" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        astro = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          condition = function(_, ctx)
+            return vim.endswith(ctx.filename, ".astro")
+          end,
+        },
+      },
+    },
+  },
 }
