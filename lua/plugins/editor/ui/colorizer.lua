9884aab36123ae411d416c62089a24836217945d
PFiS1737 <pfis1737@gmail.com>
2026-03-23 11:10:43 +0800

    feat(colorizer): new colorizer that better supports 'textDocument/documentColor'

--- lua/plugins/editor/ui/colorizer.lua
+++ lua/plugins/editor/ui/colorizer.lua
@@ -2,11 +2,46 @@
 ---@type LazySpec
 return {
   {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    keys = {
+      { "<leader>zc", "<cmd>ColorizerToggle<cr>", desc = "Toggle Colorizer" },
+    },
+    opts = {
+      filetypes = { "*", "!blink-cmp-menu" },
+      options = {
+        parsers = {
+          hex = {
+            rgb = false,
+            rgba = false,
+          },
+          css = true,
+          css_fn = true,
+          sass = { enable = true },
+          tailwind = {
+            enable = true,
+            update_names = true,
+            lsp = {
+              enable = true,
+              disable_document_color = true,
+            },
+          },
+        },
+        -- display = {
+        --   mode = "virtualtext",
+        --   virtualtext = {
+        --     char = "■",
+        --     position = "before",
+        --   },
+        -- },
+      },
+    },
+  },
+  {
     "uga-rosa/ccc.nvim",
     keys = {
       { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick Color" },
       { "<leader>zC", "<cmd>CccConvert<cr>", desc = "Convert color" },
-      { "<leader>zc", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle colorizer" },
     },
     opts = function()
       local ccc = require("ccc")
