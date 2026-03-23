13c0225147b50d351d1c2a4d7bf74a80b684ca1d
PFiS1737 <pfis1737@gmail.com>
2026-03-23 11:11:43 +0800

    feat(tailwind): use LazyVim builtin tailwind extra

--- lua/plugins/coding/lang/tailwind.lua
+++ lua/plugins/coding/lang/tailwind.lua
@@ -1,44 +1,19 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
   {
-    "luckasRanarison/tailwind-tools.nvim",
-    build = ":UpdateRemotePlugins",
-    event = "LazyFile",
+    "saghen/blink.cmp",
     opts = {
-      conceal = {
-        -- enabled = true,
-        min_length = 40,
-        symbol = "󱏿",
-      },
-      server = {
-        settings = {},
+      sources = {
+        providers = {
+          lsp = {
+            opts = {
+              tailwind_color_icon = "■ ",
+            },
+          },
+        },
       },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tailwindcss = {},
-      },
-      setup = {
-        -- tailwind-tools.nvim will setup the server.
-        tailwindcss = function()
-          return true
-        end,
-      },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    optional = true,
-    opts = function(_, opts)
-      local format = opts.formatting.format
-
-      opts.formatting.format = function(entry, item)
-        return format(entry, require("tailwind-tools.cmp").lspkind_format(entry, item))
-      end
-    end,
   },
 }
