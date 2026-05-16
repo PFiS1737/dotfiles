96211a4deb5be5ab4cd33259ff9badc0679b8f35
PFiS1737 <pfis1737@gmail.com>
2026-05-16 16:18:46 +0800

    feat(colorizer): remove nvim-colorizer.lua plugin
    Use nvim 0.12 builtin document color instead

--- lua/plugins/editor/ui/colorizer.lua
+++ lua/plugins/editor/ui/colorizer.lua
@@ -2,45 +2,6 @@
 ---@type LazySpec
 return {
   {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    keys = {
-      { "<leader>zc", "<cmd>ColorizerToggle<cr>", desc = "Toggle Colorizer" },
-    },
-    opts = {
-      filetypes = { "*", "!blink-cmp-menu" },
-      options = {
-        parsers = {
-          hex = {
-            rgb = false,
-            rgba = false,
-            hash_aarrggbb = false,
-            aarrggbb = false,
-          },
-          names = { enable = false },
-          css = true,
-          css_fn = true,
-          sass = { enable = true },
-          tailwind = {
-            enable = true,
-            update_names = true,
-            lsp = {
-              enable = true,
-              disable_document_color = true,
-            },
-          },
-        },
-        -- display = {
-        --   mode = "virtualtext",
-        --   virtualtext = {
-        --     char = "■",
-        --     position = "before",
-        --   },
-        -- },
-      },
-    },
-  },
-  {
     "uga-rosa/ccc.nvim",
     keys = {
       { "<leader>zp", "<cmd>CccPick<cr>", desc = "Pick Color" },
