caff3201e2422babfddb85bf6d0e2acf80251768
PFiS1737 <pfis1737@gmail.com>
2026-09-08 17:06:24 +0800

    fix: don't add latex_symbols auto-completions
    It's in 'lazyvim.plugins.extras.lang.julia'

--- lua/plugins/coding/lang/julia.lua
+++ lua/plugins/coding/lang/julia.lua
@@ -1,7 +1,6 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  { import = "lazyvim.plugins.extras.lang.julia" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "julia" } },
@@ -21,6 +20,13 @@ return {
           -- To update an existing install, use the following command:
           -- $ julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()'
           julia_env_path = "~/.julia/environments/nvim-lspconfig",
+          settings = {
+            -- use the same default settings as the Julia VS Code extension
+            julia = {
+              completionmode = "qualify",
+              lint = { missingrefs = "none" },
+            },
+          },
         },
       },
     },
